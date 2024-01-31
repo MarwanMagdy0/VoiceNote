47f32130e55cfbe7852eb5b04d347e2c070a5ec4
Marwan Magdy <engmarwan232@gmail.com>
2024-01-31 14:44:44 +0200

    solve some issues and make card single click

--- group.py
+++ group.py
@@ -1,13 +1,15 @@
 from vocie_widgets import *
 from image_widgets import *
-from text_widgets  import * 
+from text_widgets import *
+
 
 class Group(QWidget):
     activate_mainwindow = pyqtSignal()
-    group_is_deleted    = pyqtSignal(str)
+    group_is_deleted = pyqtSignal(str)
+
     def __init__(self, main_window, group_fname, root, parent_tree):
         super().__init__(main_window)
-        self.parent_tree =parent_tree
+        self.parent_tree = parent_tree
         self.root = root
         self.main_window = main_window
         self.workspace_json_file = main_window.workspace_json_file
@@ -15,15 +17,13 @@ class Group(QWidget):
         self.group_title = self.workspace_json_file[self.group_fname]["group-title"]
         self.init_ui()
 
-
     def init_ui(self):
-        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/group.ui",self)
+        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/group.ui", self)
         self.title_label = EditableLabel(self, self.group_title)
         self.label_layout.addWidget(self.title_label)
         self.title_label.title_updated.connect(self.editing_title_finished)
         self.add_button.clicked.connect(self.add_button_method)
         self.delete_group_button.clicked.connect(self.delete_group_method)
-
 
     def add_button_method(self):
         add_items_window = AddDialoge(self, self.group_fname)
@@ -31,16 +31,16 @@ class Group(QWidget):
         add_items_window.img_from_clib_added.connect(self.add_image)
         add_items_window.separator_is_added.connect(self.add_separator)
         add_items_window.font_changed.connect(self.add_text)
-        add_items_window.image_is_saved_from_camera.connect(self.add_image_from_camera_method)
+        add_items_window.image_is_saved_from_camera.connect(
+            self.add_image_from_camera_method
+        )
         add_items_window.setWindowModality(Qt.ApplicationModal)
         add_items_window.show()
-
 
     def add_record(self, audio_fname):
         QTreeWidgetItem(self.parent_tree, [audio_fname])
         voice_object = VoiceNote(self, self.group_fname, audio_fname)
         self.add_widget(voice_object)
-
 
     def add_image(self):
         clipboard = QApplication.clipboard()
@@ -51,25 +51,36 @@ class Group(QWidget):
             image = mime_data.imageData()
             img_widget = ImageWidget(self, self.group_fname, image_fname)
             img_widget.set_img(image)
-            image.save(self.workspace_json_file.file_directory + "/" + self.group_fname+ "/" + image_fname, "PNG")
+            image.save(
+                self.workspace_json_file.file_directory
+                + "/"
+                + self.group_fname
+                + "/"
+                + image_fname,
+                "PNG",
+            )
             self.add_widget(img_widget)
             data = self.workspace_json_file.read_data()
             data[self.group_fname]["items"].append(image_fname)
             self.workspace_json_file.save_data(data)
-    
 
     def add_image_from_camera_method(self, image_fname):
-            QTreeWidgetItem(self.parent_tree, [image_fname])
-            img_widget = ImageWidget(self, self.group_fname, image_fname)
-            img_widget.load_img(self.workspace_json_file.file_directory + "/" + self.group_fname + "/" + image_fname)
-            self.add_widget(img_widget)
-
+        QTreeWidgetItem(self.parent_tree, [image_fname])
+        img_widget = ImageWidget(self, self.group_fname, image_fname)
+        img_widget.load_img(
+            self.workspace_json_file.file_directory
+            + "/"
+            + self.group_fname
+            + "/"
+            + image_fname
+        )
+        self.add_widget(img_widget)
 
     def add_text(self, font, text, is_centered):
         if font == "null" and text == "null" and is_centered == False:
             self.activate_mainwindow.emit()
             return
-        
+
         ref2text = get_time() + ".ref"
         QTreeWidgetItem(self.parent_tree, [text])
         normal_text = NormalText(self, text, font, self.group_fname, ref2text)
@@ -78,51 +89,63 @@ class Group(QWidget):
         self.add_widget(normal_text)
         data = self.workspace_json_file.read_data()
         data[self.group_fname]["items"].append(ref2text)
-        data[self.group_fname]["refrences"][ref2text] = {"text":text, "font":font, "is-centered":is_centered}
+        data[self.group_fname]["refrences"][ref2text] = {
+            "text": text,
+            "font": font,
+            "is-centered": is_centered,
+        }
         self.workspace_json_file.save_data(data)
 
-    
     def add_separator(self):
         separator_name = get_time() + ".separator"
         self.add_widget(QHSeparationLine(self.group_fname, separator_name))
         data = self.workspace_json_file.read_data()
         data[self.group_fname]["items"].append(separator_name)
         self.workspace_json_file.save_data(data)
-    
 
     def load_data(self):
         self.title_label.title_label.setText(self.group_title)
         for item in self.workspace_json_file[self.group_fname]["items"]:
             if item.endswith(".ref"):
-                text = self.workspace_json_file[self.group_fname]["refrences"][item]["text"]
-                font = self.workspace_json_file[self.group_fname]["refrences"][item]["font"]
-                is_centered = self.workspace_json_file[self.group_fname]["refrences"][item]["is-centered"]
+                text = self.workspace_json_file[self.group_fname]["refrences"][item][
+                    "text"
+                ]
+                font = self.workspace_json_file[self.group_fname]["refrences"][item][
+                    "font"
+                ]
+                is_centered = self.workspace_json_file[self.group_fname]["refrences"][
+                    item
+                ]["is-centered"]
                 QTreeWidgetItem(self.parent_tree, [text])
                 normal_text = NormalText(self, text, font, self.group_fname, item)
                 if is_centered:
                     normal_text.label.setAlignment(Qt.AlignCenter)
                 self.add_widget(normal_text)
-            
-            elif (item.endswith(".wav") or item.endswith(".mp3")):
+
+            elif item.endswith(".wav") or item.endswith(".mp3"):
                 QTreeWidgetItem(self.parent_tree, [item])
                 voice_object = VoiceNote(self, self.group_fname, item)
                 self.add_widget(voice_object)
 
-            elif (item.endswith(".png") or item.endswith(".jpg")):
+            elif item.endswith(".png") or item.endswith(".jpg"):
                 QTreeWidgetItem(self.parent_tree, [item])
                 img_widget = ImageWidget(self, self.group_fname, item)
-                img_widget.load_img(self.workspace_json_file.file_directory + "/" + self.group_fname  + "/" +item)
+                img_widget.load_img(
+                    self.workspace_json_file.file_directory
+                    + "/"
+                    + self.group_fname
+                    + "/"
+                    + item
+                )
                 self.add_widget(img_widget)
-            
+
             elif item.endswith(".separator"):
                 separator_object = QHSeparationLine(self.group_fname, item)
                 self.add_widget(separator_object)
-    
 
     def add_widget(self, widget):
         self.box_vlayout.addWidget(widget)
         self.activate_mainwindow.emit()
-
 
     def editing_title_finished(self, new_group_title):
         self.root.setText(0, new_group_title)
@@ -131,18 +154,21 @@ class Group(QWidget):
         data[self.group_fname]["group-title"] = new_group_title
         self.workspace_json_file.save_data(data)
         self.title_label.title_label.setText(new_group_title)
-    
+
     def delete_group_method(self):
         if message_box():
-            shutil.rmtree(self.workspace_json_file.file_directory + "/" + self.group_fname)
+            shutil.rmtree(
+                self.workspace_json_file.file_directory + "/" + self.group_fname
+            )
             data = self.workspace_json_file.read_data()
             data.pop(self.group_fname)
             self.workspace_json_file.save_data(data)
             self.deleteLater()
             self.group_is_deleted.emit(self.group_fname)
 
+
 class AddDialoge(QDialog):
-    voice_record_added  = pyqtSignal(str)
+    voice_record_added = pyqtSignal(str)
     img_from_clib_added = pyqtSignal()
     font_changed = pyqtSignal(str, str, bool)
     separator_is_added = pyqtSignal()
@@ -152,7 +178,7 @@ class AddDialoge(QDialog):
         super().__init__(parent_group)
         self.group_fname = group_fname
         self.workspace_json_file = parent_group.workspace_json_file
-        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/add_items_to_group.ui",self)
+        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/add_items_to_group.ui", self)
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowContextHelpButtonHint)
         self.img_from_clib.setLayoutDirection(Qt.RightToLeft)
         self.img_from_camera.setLayoutDirection(Qt.RightToLeft)
@@ -169,45 +195,53 @@ class AddDialoge(QDialog):
     def record_audio_method(self):
         self.close()
         audio_recorder = RecordAudio(self, self.group_fname)
-        audio_recorder.audio_added.connect(lambda audio_name: self.voice_record_added.emit(audio_name))
+        audio_recorder.audio_added.connect(
+            lambda audio_name: self.voice_record_added.emit(audio_name)
+        )
         audio_recorder.setWindowModality(Qt.ApplicationModal)
         audio_recorder.show()
-    
+
     def add_clipboard_image(self):
         self.img_from_clib_added.emit()
         self.close()
-    
+
     def add_text_method(self):
         self.close()
         add_text = AddText(self)
-        add_text.font_changed.connect(lambda font, text, is_centered : self.font_changed.emit(font, text, is_centered))
+        add_text.font_changed.connect(
+            lambda font, text, is_centered: self.font_changed.emit(
+                font, text, is_centered
+            )
+        )
         add_text.setWindowModality(Qt.ApplicationModal)
         add_text.show()
 
     def add_camera_img_method(self):
-        self.close() 
+        self.close()
         image_fname = get_time() + ".png"
         camera_capture = CameraCapture(self, self.group_fname)
-        camera_capture.image_is_saved_from_camera.connect(self.image_is_saved_from_camera)
+        camera_capture.image_is_saved_from_camera.connect(
+            self.image_is_saved_from_camera
+        )
         camera_capture.setWindowModality(Qt.ApplicationModal)
         camera_capture.show()
-        
+
     def add_separator_method(self):
         self.separator_is_added.emit()
         self.close()
 
 
-
 class EditableLabel(QWidget):
     title_updated = pyqtSignal(str)
-    def __init__(self, group_object, title, style_sheet = ""):
+
+    def __init__(self, group_object, title, style_sheet=""):
         super().__init__(group_object)
-        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/label_edit.ui",self)
+        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/label_edit.ui", self)
         self.title_label.setText(title)
         self.line_edit.editingFinished.connect(self.finish_editing)
         self.line_edit.hide()
-        self.setStyleSheet(style_sheet + 'font: 63 16pt "JetBrains Mono";')
-        
+        self.setStyleSheet(style_sheet + 'font: 63 16pt "DejaVu Sans Mono";')
+
     def mouseDoubleClickEvent(self, event):
         if self.line_edit.isHidden():
             self.title_label.hide()
@@ -218,7 +252,7 @@ class EditableLabel(QWidget):
             self.title_label.setText(self.line_edit.text())
             self.title_label.show()
             self.line_edit.hide()
-    
+
     def finish_editing(self):
         new_text = self.line_edit.text()
         self.line_edit.hide()
--- workspace.py
+++ workspace.py
@@ -1,21 +1,21 @@
 from starting_screen import *
 from group import EditableLabel
 
+
 class Card(QWidget):
     card_selected = pyqtSignal(dict, str)
-    def __init__(self, card_data : dict, workspace_file_directory):
+
+    def __init__(self, card_data: dict, workspace_file_directory):
         super().__init__()
-        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/group_card.ui",self)
+        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/group_card.ui", self)
         j = HandleJsonFiles(workspace_file_directory + "/" + card_data["card-fname"])
         self.label.setText(j["card-title"])
         self.card_data = card_data
         self.workspace_file_directory = workspace_file_directory
-    
 
-    def mouseDoubleClickEvent(self, event):
+    def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.card_selected.emit(self.card_data, self.workspace_file_directory)
-
 
 
 class WorkSpace(QWidget):
@@ -23,30 +23,38 @@ class WorkSpace(QWidget):
     gridLayout: QGridLayout
     card_selected = pyqtSignal(dict, str)
     label_layout: QVBoxLayout
-    go_back_button:QPushButton
+    go_back_button: QPushButton
     go_back_from_workspace = pyqtSignal(dict, str)
     go_back_to_welcome_screen = pyqtSignal()
+
     def __init__(self, workspace_file):
         super().__init__()
         self.workspace_file = workspace_file
         self.workspace_json_file = HandleJsonFiles(workspace_file)
         self.init_ui()
 
-
     def init_ui(self):
-        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/workspace.ui",self)
+        uic.loadUi(SCRIPT_DIRECTORY + "/" + "ui/workspace.ui", self)
         self.add_new_card_button.clicked.connect(self.add_new_card_method)
         self.next_item_index = 0
         self.scroll_area_bar = self.scrollArea.verticalScrollBar()
-        self.title_label = EditableLabel(self, self.workspace_json_file.read_data()["card-title"], 'background:#1e1e1e;')
+        self.title_label = EditableLabel(
+            self,
+            self.workspace_json_file.read_data()["card-title"],
+            "background:#1e1e1e;",
+        )
         self.label_layout.addWidget(self.title_label)
         self.title_label.title_updated.connect(self.editing_title_finished)
         back_directory = self.workspace_json_file.read_data()["back"]
         if back_directory is not None:
             j = HandleJsonFiles(self.workspace_file[:-6] + back_directory)
             file_type = j.read_data()["type"]
-            prev_card_data = {"card-fname":back_directory, "type":file_type}
-            self.go_back_button.clicked.connect(lambda : self.go_back_from_workspace.emit(prev_card_data, self.workspace_file[:-6]))
+            prev_card_data = {"card-fname": back_directory, "type": file_type}
+            self.go_back_button.clicked.connect(
+                lambda: self.go_back_from_workspace.emit(
+                    prev_card_data, self.workspace_file[:-6]
+                )
+            )
         else:
             self.go_back_button.clicked.connect(self.go_back_to_welcome_screen.emit)
             data = refrences_file.read_data()
@@ -55,54 +63,76 @@ class WorkSpace(QWidget):
             data["ref"].insert(0, os.path.abspath(self.workspace_file))
             refrences_file.save_data(data)
 
-
         for card_data in self.workspace_json_file["cards"]:
             card = Card(card_data, self.workspace_file[:-6])
-            card.card_selected.connect(lambda card_directory, workspace_directroy: self.card_selected.emit(card_directory, workspace_directroy))
+            card.card_selected.connect(
+                lambda card_directory, workspace_directroy: self.card_selected.emit(
+                    card_directory, workspace_directroy
+                )
+            )
             self.add_widget(card)
         self.layout = self.scrollArea.widget().layout()
         self.scrollArea.widget().setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         self.scroll_area_bar = self.scrollArea.verticalScrollBar()
-
 
     def add_new_card_method(self):
         self.starting_screen = StartingScreen()
         self.starting_screen.setWindowModality(Qt.ApplicationModal)
         self.starting_screen.file_type_choosed.connect(self.create_card_directory)
         self.starting_screen.show()
-    
 
     def create_card_directory(self, file_type):
-        card_fname     = get_time()
+        card_fname = get_time()
         card_directory = os.path.abspath(self.workspace_file[:-6] + "/" + card_fname)
-        card_data = {"card-fname" : card_fname + "/" + card_fname + ".vnote", "type":file_type}
+        card_data = {
+            "card-fname": card_fname + "/" + card_fname + ".vnote",
+            "type": file_type,
+        }
         os.mkdir(card_directory)
         os.mkdir(card_directory + "/" + card_fname)
         j = HandleJsonFiles(card_directory + "/" + card_fname + ".vnote")
         if file_type == "workspace":
-            j.save_data({"type":"workspace", "cards":[], "back":"/../../../" + os.path.basename(self.workspace_file), "card-title":"workspace"})
+            j.save_data(
+                {
+                    "type": "workspace",
+                    "cards": [],
+                    "back": "/../../../" + os.path.basename(self.workspace_file),
+                    "card-title": "workspace",
+                }
+            )
         else:
-            j.save_data({"type":"file-view", "back": "/../../../" + os.path.basename(self.workspace_file), "card-title":"file view"})
+            j.save_data(
+                {
+                    "type": "file-view",
+                    "back": "/../../../" + os.path.basename(self.workspace_file),
+                    "card-title": "file view",
+                }
+            )
 
         card = Card(card_data, self.workspace_file[:-6])
-        card.card_selected.connect(lambda card_directory, workspace_directroy: self.card_selected.emit(card_directory, workspace_directroy))
+        card.card_selected.connect(
+            lambda card_directory, workspace_directroy: self.card_selected.emit(
+                card_directory, workspace_directroy
+            )
+        )
         self.add_widget(card)
         data = self.workspace_json_file.read_data()
         data["cards"].append(card_data)
         self.workspace_json_file.save_data(data)
 
     def add_widget(self, widget):
-        self.gridLayout.addWidget(widget, self.next_item_index//5, self.next_item_index%5)
-        self.scroll_area_bar.setMaximum(self.scroll_area_bar.maximum()+200)
+        self.gridLayout.addWidget(
+            widget, self.next_item_index // 5, self.next_item_index % 5
+        )
+        self.scroll_area_bar.setMaximum(self.scroll_area_bar.maximum() + 200)
         self.scroll_area_bar.setValue(self.scroll_area_bar.maximum())
-        self.next_item_index+=1
-    
+        self.next_item_index += 1
+
     def editing_title_finished(self, new_name):
         data = self.workspace_json_file.read_data()
         data["card-title"] = new_name
         self.workspace_json_file.save_data(data)
         self.title_label.title_label.setText(new_name)
-
 
 
 if __name__ == "__main__":
